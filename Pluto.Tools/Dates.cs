1d067919d96e97e2d4c9f67215d47efebdfd3b9a
elpluto <rhernandez@rhsoft.es>
2012-02-18 05:58:48 +0100

    Refactor stylecop revision 1

---
+++ Pluto.Tools/Dates.cs
@@ -0,0 +1,25 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Dates.cs" company="">
+// DOC: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Pluto.Tools
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+
+	/// <summary>
+	/// Helper class to dates operations.
+	/// </summary>
+	public class Dates
+	{
+		public static int GetDatesDiffInDays(DateTime firstDate, DateTime lastDate)
+		{
+			return (lastDate - firstDate).Days;
+		}
+	}
+
+}
