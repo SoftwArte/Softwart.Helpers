815db0c3575435ceb554c46410902544ac640329
Rafa Hernández <rafa.hernandez@rhsoft.es>
2014-02-14 18:15:55 +0100

    Add untracked files.

---
+++ Network/Mail.cs
@@ -0,0 +1,39 @@
+﻿/*
+*
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Xml;
+
+
+namespace Pluto.Tools
+{
+	public class Mail
+	{
+		/// <summary>
+		/// Send a simple mail message to a single mail address.
+		/// </summary>
+		/// <param name="message"></param>
+		/// <param name="subject"></param>
+		/// <param name="address"></param>
+		public static void SendMessage(string message, string subject, string address)
+		{
+			var mail = new MailMessage();
+
+			mail.To.Add(new MailAddress(address));
+			mail.Subject = subject;
+			mail.Body = message;
+
+			var cliente = new SmtpClient();
+			cliente.Send(mail);
+		}
+	}
+}
+
+
---
+++ Network/WebBrowse.cs
@@ -0,0 +1,78 @@
+﻿/*
+*
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+
+namespace Pluto.Tools
+{
+	public class WebBrowse
+	{
+		private static WebBrowser client;
+
+		public WebBrowse()
+		{
+			BrowserState = BrowserPageStateEnum.Ready;
+			HttpMethod = "GET";
+			Body = "";
+		}
+
+		public string SourceCode { get; set; }
+
+		public HtmlDocument BrowserPage { get; set; }
+
+		public BrowserPageStateEnum BrowserState { get; set; }
+
+		public string Url { get; set; }
+
+		public string HttpMethod { get; set; }
+
+		public string Body { get; set; }
+
+		//Browse a page.
+		public void GetBrowseWebPage()
+		{
+			BrowserState = BrowserPageStateEnum.Downloading;
+			client = new WebBrowser();
+			client.Visible = true;
+			client.ScriptErrorsSuppressed = true;
+			client.AllowNavigation = true;
+			client.DocumentCompleted += ClientOnDocumentCompleted;
+			if(HttpMethod == "GET")
+			{
+				//Get a page using GET.
+				client.Navigate(Url);
+			}
+			else
+			{
+				//Return a page using POST.
+				client.Navigate(Url, "_self", Encoding.UTF8.GetBytes(Body), "Content-Type=pplication/x-www-form-urlencoded");
+			}
+		}
+		//Handler.
+		private void ClientOnDocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+		{
+			if(e.Url != null)
+			{
+				BrowserState = BrowserPageStateEnum.Completed;
+				BrowserPage = client.Document;
+				SourceCode = client.DocumentText;
+			}
+		}
+
+		public enum BrowserPageStateEnum
+		{
+			Ready, Downloading, Completed
+		}
+	}
+}
+
+
