e29d610718cb9ca2734542889cabc83227f84891
Rafa Hernández <rafa.hernandez@rhsoft.es>
2014-03-13 09:20:03 +0100

    Add headers support  to GetWebPage method.

--- Network/Web.cs
+++ Network/Web.cs
@@ -8,112 +8,122 @@
 
 namespace Softwarte.Helpers
 {
-	using System;
-	using System.Drawing;
-	using System.IO;
-	using System.Net;
-	using System.Text;
+  using System;
+  using System.Drawing;
+  using System.IO;
+  using System.Net;
+  using System.Text;
 
   public enum HttpMethodEnum
   {
     GET, POST
   }
-	public class WebHelper
-	{
-    
-		/// <summary>
-		///	Download a web page as string, better support for encodings. 
-		/// </summary>
-		/// <param name="url"></param>
-		/// <returns></returns>
-		public static string GetWebPage(string url, HttpMethodEnum httpMethod , string body = "")
-		{
-			WebClient client = new WebClient();
-			//
-			if(httpMethod == HttpMethodEnum.GET)
-			{
-				//Return a page using GET.
-				return client.DownloadString(url);
-			}
-			else
-			{
-				//Return a page using POST.
-				client.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
-				return Encoding.UTF8.GetString(client.UploadData(url, "POST", Encoding.UTF8.GetBytes(body)));
-			}
-		}
+  public class WebHelper
+  {
 
-		/// <summary>
-		/// Download a web page as stream, is neccesary read the strema to return the content as a string.
-		/// </summary>
-		/// <param name="url"></param>
-		/// <returns></returns>
-		public static string GetPage(Uri url)
-		{
-			WebRequest request = WebRequest.Create(url);
-			StreamReader response = new StreamReader(request.GetResponse().GetResponseStream());
-			StringBuilder sb = new StringBuilder();
-			string line;
+    /// <summary>
+    ///	Download a web page as string, better support for encodings. 
+    /// </summary>
+    /// <param name="url"></param>
+    /// <returns></returns>
+    public static string GetWebPage(string url, HttpMethodEnum httpMethod, string body = "", params string[] headers)
+    {
+      WebClient client = new WebClient();
+      //
+      if (httpMethod == HttpMethodEnum.GET)
+      {
+        //Add headers is passed
+        foreach (var header in headers)
+        {
+          client.Headers.Add(header);
+        }
+        //Return a page using GET.
+        return client.DownloadString(url);
+      }
+      else
+      {
+        //Add headers is passed
+        foreach (var header in headers)
+        {
+          client.Headers.Add(header);
+        }
+        //Return a page using POST. A content-type header at least in needed.
+        client.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+        return Encoding.UTF8.GetString(client.UploadData(url, "POST", Encoding.UTF8.GetBytes(body)));
+      }
+    }
 
-			// Copy the response stream 
-			while((line = response.ReadLine()) != null)
-			{
-				if(line.Length > 0)
-					sb.Append(line);
-			}
+    /// <summary>
+    /// Download a web page as stream, is neccesary read the strema to return the content as a string.
+    /// </summary>
+    /// <param name="url"></param>
+    /// <returns></returns>
+    public static string GetPage(Uri url)
+    {
+      WebRequest request = WebRequest.Create(url);
+      StreamReader response = new StreamReader(request.GetResponse().GetResponseStream());
+      StringBuilder sb = new StringBuilder();
+      string line;
 
-			response.Close();
-			return sb.ToString();
-		}
-		/// <summary>
-		/// Download and return a binary representation of a web resource as byte array.
-		/// </summary>
-		/// <remarks>if cant download by exception, return empty byte[] array. </remarks>
-		/// <returns></returns>
-		public static byte[] GetWebResourceBinary(string url)
-		{
-			using(var client = new WebClient())
-			{
-				try
-				{
-					return client.DownloadData(url);
-				}
-				catch(Exception ex)
-				{
-					return new byte[] { };
-				}
-			}
+      // Copy the response stream 
+      while ((line = response.ReadLine()) != null)
+      {
+        if (line.Length > 0)
+          sb.Append(line);
+      }
+
+      response.Close();
+      return sb.ToString();
+    }
+    /// <summary>
+    /// Download and return a binary representation of a web resource as byte array.
+    /// </summary>
+    /// <remarks>if cant download by exception, return empty byte[] array. </remarks>
+    /// <returns></returns>
+    public static byte[] GetWebResourceBinary(string url)
+    {
+      using (var client = new WebClient())
+      {
+        try
+        {
+          return client.DownloadData(url);
+        }
+        catch (Exception ex)
+        {
+          return new byte[] { };
+        }
+      }
 
 
-		}
-		/// <summary>
-		/// Download and return an image from url.
-		/// </summary>
-		/// <param name="url"></param>
-		/// <returns></returns>
-		public static Image GetWebImage(string url)
-		{
-			using(var client = new WebClient())
-			{
-				byte[] resource = client.DownloadData(url);
-				using(var str = new MemoryStream())
-				{
-					str.Write(resource, 0, resource.Length);
-					return Image.FromStream(str);
-				}
-			}
+    }
+    /// <summary>
+    /// Download and return an image from url.
+    /// </summary>
+    /// <param name="url"></param>
+    /// <returns></returns>
+    public static Image GetWebImage(string url)
+    {
+      using (var client = new WebClient())
+      {
+        byte[] resource = client.DownloadData(url);
+        using (var str = new MemoryStream())
+        {
+          str.Write(resource, 0, resource.Length);
+          return Image.FromStream(str);
+        }
+      }
 
-		}
-		/// <summary>
-		/// Compound an absolute url with a base url and a url part.
-		/// </summary>
-		/// <param name="baseUrl"></param>
-		/// <param name="urlFragment"></param>
-		/// <returns></returns>
-		public static string GetAbsoluteUrl(string baseUrl, string urlFragment)
-		{
-			return new Uri(new Uri(baseUrl), urlFragment).ToString();
-		}
-	}
+    }
+    /// <summary>
+    /// Compound an absolute url with a base url and a url part.
+    /// </summary>
+    /// <param name="baseUrl"></param>
+    /// <param name="urlFragment"></param>
+    /// <returns></returns>
+    public static string GetAbsoluteUrl(string baseUrl, string urlFragment)
+    {
+      return new Uri(new Uri(baseUrl), urlFragment).ToString();
+    }
+  }
 
 }
