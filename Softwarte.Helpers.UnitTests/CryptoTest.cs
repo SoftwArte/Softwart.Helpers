a5cd1379ce087e1b08ea428d37a6f1202812b128
Rafa Hernández <rafa.hernandez@rhsoft.es>
2014-04-22 10:51:26 +0200

    AES fixes.

---
+++ Softwarte.Helpers.UnitTests/CryptoTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Softwarte.Helpers.UnitTests
+{
+  [TestClass]
+  public class CryptoTest
+  {
+    [TestMethod]
+    public void EncryptAesTest()
+    {
+      var input = "This is my sample text for AES encryptiobn test.";
+      var result = CryptoHelper.EncryptStrongAes(input, "aEyf7TSUad@94D#@y784");
+    }
+    [TestMethod]
+    public void DecryptAesTest()
+    {
+      var input = "This is my sample text for AES encryptiobn test.";
+      var encryptedInput = CryptoHelper.EncryptStrongAes(input, "aEyf7TSUad@94D#@y784");
+      var result = CryptoHelper.DecryptStrongAes(encryptedInput, "aEyf7TSUad@94D#@y784");
+      Assert.IsTrue(result == input);
+    }
+  }
+}
